a9f7baf3ef2cdb753cc52a1521195509303887c2
Nitu170287 <108494260+Nitu170287@users.noreply.github.com>
2023-05-23 05:42:19 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // your code here
+
+const button = document.getElementById("button");
+const url = document.getElementById("url");
+const name = document.getElementById("name");
+const year = document.getElementById("year");
+
+button.addEventListener("click" , (event) => {
+	event.preventDefault();
+	url.append(name.value);
+	url.append(year.value);
+	
+})
